3f5b1a6b726009b3c07323d3fe603fed40ebb952
Fatin <mdfatin35@gmail.com>
2021-10-07 01:55:03 +0600

    Flutter 2.0 practice

---
+++ lib/Splash_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 450,
+          width: 350,
+          margin: EdgeInsets.only(left: 60,right:40,),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height/3,
+                width: MediaQuery.of(context).size.width/3,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Color.fromRGBO(41, 8, 102,1),
+                    width: 4,
+                    style: BorderStyle.solid,
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage("images/Cover_Image.png"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height/4,
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Welcome To flutter advance project. It is an ICTD Project. Iet's enter in it.",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.grey.shade700,
+                    decoration: TextDecoration.underline,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                height: 55,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width:3,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context)=>HomePage(),
+                      ),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Enter"),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_2/Splash_screen.dart';
+import 'package:flutter_2/edit_page.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context)=>SplashScreen(),
+              ),
+            );
+          },
+          icon: Icon(Icons.arrow_back_ios_outlined),
+          iconSize: 20,
+        ),
+        actions: [
+          InkWell(
+            onTap: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)=> EditPage(),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                height: 12,
+                width: 80,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 3,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.black45,
+                ),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Edit"),
+                      Icon(Icons.create_outlined,size: 15,),
+                    ]
+                ),
+              ),
+            ),
+          ),
+        ],
+        title: Text("Personal Info"),
+        centerTitle: true,
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+
+              // User name and bio Container start
+              Container(
+                height: MediaQuery.of(context).size.height/4,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+
+                    // Profile Image Container
+                    Container(
+                      height: MediaQuery.of(context).size.height/7,
+                      width: MediaQuery.of(context).size.width/4,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: AssetImage("images/Profile_Image.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                        border: Border.all(
+                          color: Color.fromRGBO(52, 95, 97,1),
+                          width: 3,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                    ),
+
+                    // Address And Bio Container
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.5,
+                      height: MediaQuery.of(context).size.height/7,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: RichText(
+                          textAlign: TextAlign.justify,
+                          text: TextSpan(
+                              text: "Saied Ahammed Foyez\n",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black45,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: "217/A/1,South Perirbagh,Mirpur-2,Dhaka-1216",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                ),
+                              ]
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
